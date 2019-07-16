c40564aac24cded056a13f65d19e4f7ae9b52eb0
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-16 15:32:32 +0200

    Aggiunta vari commenti.

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 // Creare un hamburger menu con l’utilizzo di Html, Css e JavaScript.
 
-
+// Disposizione generale file js
 $( document ).ready(function() {
 
-
+  // comando per far comparire menu dopo click su tasto hamburger
+  // selezione contenitore
   $(".header-right > a").click(function() {
-
-      $(".hamburger-menu").show(1000);
+    // selezione di cosa mostrare dopo il click
+      $(".hamburger-menu").show(500);
     });
 
 
-
+    // comando per far sparire menu dopo click su tasto hamburger
+    // selezione del contenitore
     $(".hamburger-menu > .close").click(function() {
-
+      // selezione di cosa far scomparire
         $(".hamburger-menu").hide(200);
       });
 
